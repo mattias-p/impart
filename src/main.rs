a29f35ff9116d82feac3bdb1598d02a12c3da52c
Mattias Päivärinta <mattias.paivarinta@iis.se>
2022-07-21 21:59:54 +0200

    Add a couple of CLI params

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[clap(version, about)]
+struct Cli {
+    /// Width in modules (x-coordinates)
+    #[clap(short, long, default_value_t = 256)]
+    width: u16,
+
+    /// Height in modules (z-coordinates)
+    #[clap(short, long, default_value_t = 256)]
+    height: u16,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    let width = cli.width;
+    let height = cli.height;
+
+    println!("width {width}");
+    println!("height {height}");
 }
