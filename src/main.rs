ae73e4046ce6cacebaed4be516705feacfc5d002
Mattias Päivärinta <mattias.paivarinta@iis.se>
2022-07-21 22:19:54 +0200

    Write a PNG file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::fs::File;
+use std::io::BufWriter;
+use std::path::PathBuf;
+
 use clap::Parser;
 
 #[derive(Parser)]
@@ -10,13 +14,21 @@ struct Cli {
     /// Height in modules (z-coordinates)
     #[clap(short, long, default_value_t = 256)]
     height: u16,
+
+    /// Output file
+    #[clap(short, long, default_value = "out.png")]
+    outfile: PathBuf,
 }
 
 fn main() {
     let cli = Cli::parse();
-    let width = cli.width;
-    let height = cli.height;
 
-    println!("width {width}");
-    println!("height {height}");
+    let file = File::create(cli.outfile).unwrap();
+    let ref mut w = BufWriter::new(file);
+
+    let data = vec![127; cli.width as usize * cli.height as usize];
+
+    let encoder = png::Encoder::new(w, cli.width as u32, cli.height as u32);
+    let mut writer = encoder.write_header().unwrap();
+    writer.write_image_data(&data).unwrap();
 }
