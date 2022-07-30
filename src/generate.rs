f8df0fd0df4b1a07fa70c4c08723670f53c50c9b
Mattias Päivärinta <mattias.paivarinta@iis.se>
2022-07-30 13:48:17 +0200

    Change levels

--- src/generate.rs
+++ src/generate.rs
@@ -100,17 +100,16 @@ impl Generator {
 
         let mut cells = Vec::with_capacity(width as usize * height as usize);
 
-        let step_x = 1.0 / (width - 1) as f64;
-        let step_y = 1.0 / (height - 1) as f64;
-        let mut y = 0.0;
+        let step_x = 2.0 / (width - 1) as f64;
+        let step_y = 2.0 / (height - 1) as f64;
+        let mut y = 1.0;
         for _ in 0..height {
-            let mut x = 0.0;
+            let mut x = -1.0;
             for _ in 0..width {
-                let elevation = ((0.5 + self.slope / 2.0)
-                    + (0.5 - self.slope / 2.0) * elevation_noise.get([x, y])
-                    - self.slope * y) as f32;
-                let humidity = (0.5 + 0.5 * humidity_noise.get([x, y])) as f32;
-                let temperature = (0.5 + 0.5 * temperature_noise.get([x, y])) as f32;
+                let elevation =
+                    ((1.0 - self.slope) * elevation_noise.get([x, y]) + self.slope * y) as f32;
+                let humidity = humidity_noise.get([x, y]) as f32;
+                let temperature = temperature_noise.get([x, y]) as f32;
 
                 cells.push(Cell {
                     vars: vec![elevation, humidity, temperature],
@@ -118,7 +117,7 @@ impl Generator {
 
                 x += step_x;
             }
-            y += step_y;
+            y -= step_y;
         }
         cells
     }
